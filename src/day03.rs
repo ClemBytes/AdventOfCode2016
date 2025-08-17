98a0281f4286bc23a0b75905cf7950e1c4573080
Clémentine Scohy <clembytes@proton.me>
2025-08-17 11:03:36 +0200

    DAY03: refactor

--- src/day03.rs
+++ src/day03.rs
@@ -19,15 +19,15 @@ pub fn run() {
     day03_part2(&example_part2, &input_part2);
 }
 
-fn parse_part1(raw_input: &str) -> Vec<Vec<u32>> {
-    let mut triangles: Vec<Vec<u32>> = vec![];
-    let re = Regex::new(r"^\ *([0-9]+)\ +([0-9]+)\ +([0-9]+)$").unwrap();
+fn parse_part1(raw_input: &str) -> Vec<[u32; 3]> {
+    let mut triangles: Vec<[u32; 3]> = vec![];
+    let re = Regex::new(r"^ *([0-9]+) +([0-9]+) +([0-9]+)$").unwrap();
     for line in raw_input.lines() {
         let matches = re.captures(line).unwrap();
-        let mut sides = vec![
-            matches[1].parse::<u32>().unwrap(),
-            matches[2].parse::<u32>().unwrap(),
-            matches[3].parse::<u32>().unwrap(),
+        let mut sides = [
+            matches[1].parse().unwrap(),
+            matches[2].parse().unwrap(),
+            matches[3].parse().unwrap(),
         ];
         sides.sort();
         triangles.push(sides);
@@ -35,25 +35,25 @@ fn parse_part1(raw_input: &str) -> Vec<Vec<u32>> {
     triangles
 }
 
-fn parse_part2(raw_input: &str) -> Vec<Vec<u32>> {
-    let mut triangles: Vec<Vec<u32>> = vec![];
-    let re = Regex::new(r"^\ *([0-9]+)\ +([0-9]+)\ +([0-9]+)$").unwrap();
-    let mut temp1: Vec<u32> = vec![];
-    let mut temp2: Vec<u32> = vec![];
-    let mut temp3: Vec<u32> = vec![];
+fn parse_part2(raw_input: &str) -> Vec<[u32; 3]> {
+    let mut triangles: Vec<[u32; 3]> = vec![];
+    let re = Regex::new(r"^ *([0-9]+) +([0-9]+) +([0-9]+)$").unwrap();
+    let mut temp1 = vec![];
+    let mut temp2 = vec![];
+    let mut temp3 = vec![];
     for (i, line) in raw_input.lines().enumerate() {
         let matches = re.captures(line).unwrap();
-        temp1.push(matches[1].parse::<u32>().unwrap());
-        temp2.push(matches[2].parse::<u32>().unwrap());
-        temp3.push(matches[3].parse::<u32>().unwrap());
+        temp1.push(matches[1].parse().unwrap());
+        temp2.push(matches[2].parse().unwrap());
+        temp3.push(matches[3].parse().unwrap());
         if i % 3 == 2 {
             temp1.sort();
             temp2.sort();
             temp3.sort();
 
-            triangles.push(temp1.clone());
-            triangles.push(temp2.clone());
-            triangles.push(temp3.clone());
+            triangles.push(temp1.clone().try_into().unwrap());
+            triangles.push(temp2.clone().try_into().unwrap());
+            triangles.push(temp3.clone().try_into().unwrap());
 
             temp1.clear();
             temp2.clear();
@@ -63,43 +63,28 @@ fn parse_part2(raw_input: &str) -> Vec<Vec<u32>> {
     triangles
 }
 
-fn constructible(triangle: &[u32]) -> bool {
+fn constructible(triangle: &[u32; 3]) -> bool {
     triangle[0] + triangle[1] > triangle[2]
 }
 
-fn day03_part1(input: &[Vec<u32>]) {
+fn day03_part1(input: &[[u32; 3]]) {
     // Exemple tests
     assert!(!constructible(&[5, 10, 25]));
 
     // Solve puzzle
-    let mut res = 0;
-    for triangle in input {
-        if constructible(triangle) {
-            res += 1;
-        }
-    }
+    let res = input.into_iter().copied().filter(constructible).count();
     println!("Result part 1: {res}");
     assert_eq!(res, 1050);
     println!("> DAY03 - part 1: OK!");
 }
 
-fn day03_part2(example: &[Vec<u32>], input: &[Vec<u32>]) {
+fn day03_part2(example: &[[u32; 3]], input: &[[u32; 3]]) {
     // Exemple tests
-    let mut res = 0;
-    for triangle in example {
-        if constructible(triangle) {
-            res += 1;
-        }
-    }
+    let res = example.into_iter().copied().filter(constructible).count();
     assert_eq!(res, 6);
 
     // Solve puzzle
-    let mut res = 0;
-    for triangle in input {
-        if constructible(triangle) {
-            res += 1;
-        }
-    }
+    let res = input.into_iter().copied().filter(constructible).count();
     println!("Result part 2: {res}");
     assert_eq!(res, 1921);
     println!("> DAY03 - part 2: OK!");
